cb4e3cfe55ff5025427124f53544a4781415eab7
AnzhelikaKlev <angelikaklev@gmail.com>
2013-05-03 16:38:35 +0800

    Areas added.

---
+++ AlisFirst/AlisFirst/Areas/LMS/LMSAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AlisFirst.Areas.LMS
+{
+    public class LMSAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "LMS";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "LMS_default",
+                "LMS/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
