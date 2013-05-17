d84d850e62532fcc2c0da1c659813a41eeca6b7b
AnzhelikaKlev <angelikaklev@gmail.com>
2013-05-17 21:05:33 +0800

    Models and their root controllers and views, dBInitializer changed to correspond to Alis approved SRS and ERD.

--- AlisFirst/AlisFirst/Models/Asset.cs
+++ AlisFirst/AlisFirst/Models/Asset.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace AlisFirst.Models
 {
@@ -11,26 +12,47 @@ namespace AlisFirst.Models
         public Asset()
         {
             CheckListItems = new List<CheckListItem>();
+            IsActive = true;
         }
 
-
         public int AssetID { get; set; }
+
+        [MaxLength(10)]
+        [Required(ErrorMessage = "Bar code is required.")]
         public string BarCode { get; set; }
+
+        [MaxLength(15)]
+        [Display(Name = "Serial Number")]
         public string SerialNum { get; set; }
 
+        [Display(Name = "Supplier")]
         public int SupplierID { get; set; }
         public virtual Supplier Supplier { get; set; }
 
+        [Display(Name = "Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+        [Display(Name = "Model")]
         public int AssetModelID { get; set; }
         public virtual AssetModel AssetModel { get; set; }
 
+        [Display(Name = "Date Recieved")]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime? DateRecieved { get; set; }
+
+        [Display(Name = "Warranty Expires")]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime? WarrantyExpires { get; set; }
+
         public decimal? PurchaseCost { get; set; }
 
+        [Display(Name = "Loanable")]        
         public bool IsLoanable { get; set; }
+        
         public string Notes { get; set; }
 
+        [ScaffoldColumn(false)]
         public bool IsActive { get; set; }
 
         public virtual ICollection<AssetCondition> AssetConditions { get; set; }
@@ -40,10 +62,9 @@ namespace AlisFirst.Models
         public virtual ICollection<AssignedTo> AssignedToes { get; set; }
         public virtual ICollection<AssignedLocation> AssignedLocations { get; set; }
 
-        //public string AssetName
-        //{
-        //    get { return string.Format("{0} {1} {2}", AssetModel.Manufacturer.ToString(), AssetModel.Category.ToString(), AssetModel.ToString()); }
-        //}
-      
+        public string Name
+        {
+            get { return string.Format("{0} {1} {2}", AssetModel.Manufacturer.ToString(), Category.ToString(), AssetModel.ToString()); }
+        }   
     }
 }
