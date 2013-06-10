55f8fb9159b6aa177aa70bf54ceb67723d3fa831
Matthew Kramara <mattkramara@gmail.com>
2013-06-10 14:32:56 +0800

    Fixed some messed up datepicker implementation in index, anybody having trouble with showing date formatted fields should take a look at how I've done the outputting of borrower expire date within /AMS/Employee/Index.cshtml.

--- AlisFirst/AlisFirst/Global.asax.cs
+++ AlisFirst/AlisFirst/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using AlisFirst.Models;
+using AlisFirst.ViewModels;
 using AlisFirst.DAL;
 using AlisFirst.Areas.LMS.ViewModels;
 using AlisFirst.Areas.AMS.ViewModels;
